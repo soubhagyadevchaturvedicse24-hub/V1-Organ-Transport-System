f7f8dbfd40d13202e508a26ed159be13d0d1bbb6
Soubhagya Dev Chaturvedi <soubhagyadevchaturvedi2005@gmail.com>
2026-07-20 21:10:30 +0530

    Initial commit: Set up project skeleton and folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', padding: '2rem', textAlign: 'center' }}>
+      <h1>Organ Transport System</h1>
+      <p>Welcome to the Human Organ Transplantation Web App</p>
+    </div>
+  );
+}
+
+export default App;
