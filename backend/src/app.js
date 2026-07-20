f936b45e7d04e60168b6a0067f9682c036254316
Soubhagya Dev Chaturvedi <soubhagyadevchaturvedi2005@gmail.com>
2026-07-20 21:58:24 +0530

    feat: Auth Controllers and Routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,16 @@ import cors from 'cors';
 import helmet from 'helmet';
 import healthRoute from './health/health.route.js';
 
+import authRoute from './auth/routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 
 // Middleware
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.get('/', (req, res) => {
@@ -18,6 +22,7 @@ app.get('/', (req, res) => {
   });
 });
 
+app.use('/api/v1/auth', authRoute);
 app.use('/api/v1', healthRoute);
 
 export default app;
